074a866918a6407b9ca3218894528a93f2486c7b
matssondev <patric@matsson.dev>
2025-12-07 23:12:38 +0100

    created player object

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ import pygame
 
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import Player
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         log_state()
@@ -14,7 +17,13 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+
+        player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+        player.draw(screen)
+
         pygame.display.flip()
+        clock.tick(60)
+        dt = clock.tick(60) / 1000.0
 
 
 if __name__ == "__main__":
